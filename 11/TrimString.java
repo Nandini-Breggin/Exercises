3cba2951d976752e3802d5aba1932a74d11f3579
Nandini Breggin <bregginnr@s.dcsdk12.org>
2020-09-13 15:15:49 -0600

    .trim() String doesn't remove interal spaces

---
+++ 11/TrimString.java
@@ -0,0 +1,20 @@
+public class TrimString
+{
+  public static void main ( String[] args )
+  {
+    
+    String first = new String("    In a Hole    in the ground there lived a Hobbit.   .");
+    
+    System.out.println(first);
+
+    String second;
+
+    second = first.trim();
+
+    System.out.println(second);
+
+    // .trim() only takes out the first set of spaces before the first letter in the "",
+    // internal spaces within "" remain.
+
+  }
+}
