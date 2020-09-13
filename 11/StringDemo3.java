0781a89c30fe8cfc4922b7096b8107ddb7d35ec8
Nandini Breggin <bregginnr@s.dcsdk12.org>
2020-09-13 16:39:24 -0600

    Errors are there with str.length

--- 11/StringDemo3.java
+++ 11/StringDemo3.java
@@ -4,8 +4,10 @@ public class StringDemo3
   {
     String str = new String( "Golf is a good walk spoiled." ); // create the original object
 
-    String sub = str.substring(0, 28); //create a new object from the original
+    //len = str.length(28);
 
+    //String sub = str.substring(len); //create a new object from the original
+    
     System.out.println( sub ); //Removes the characters of the number, keeps the number between the parameters, and the deletes the rest of the characters after the second parameter number 
 
   }
