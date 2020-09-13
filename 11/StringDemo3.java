8cfa2951c5c04f6b4e1628e051e7f291c421fad1
Nandini Breggin <bregginnr@s.dcsdk12.org>
2020-09-13 16:33:35 -0600

    First Modification: Same parameters.

--- 11/StringDemo3.java
+++ 11/StringDemo3.java
@@ -4,9 +4,9 @@ public class StringDemo3
   {
     String str = new String( "Golf is a good walk spoiled." ); // create the original object
 
-    String sub = str.substring(8); //create a new object from the original
+    String sub = str.substring(8, 8); //create a new object from the original
 
-    System.out.println( sub ); //Removes the characters of the number
+    System.out.println( sub ); //Removes the characters of the number, keeps the number between the parameters, and the deletes the rest of the characters after the second parameter number 
 
   }
 }
