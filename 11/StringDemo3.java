32f2000d6a33544a7ac7266d9e566714efc6b849
Nandini Breggin <bregginnr@s.dcsdk12.org>
2020-09-13 16:35:12 -0600

    First commit: same parameters

--- 11/StringDemo3.java
+++ 11/StringDemo3.java
@@ -4,9 +4,11 @@ public class StringDemo3
   {
     String str = new String( "Golf is a good walk spoiled." ); // create the original object
 
-    String sub = str.substring(8, 8); //create a new object from the original
+    String sub = str.substring(0, 28); //create a new object from the original
 
     System.out.println( sub ); //Removes the characters of the number, keeps the number between the parameters, and the deletes the rest of the characters after the second parameter number 
 
   }
 }
+
+// 1. Nothing shows up
