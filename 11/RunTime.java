e345a61b41bffeeed38daa4c0dea8bb1d4ae0bea
Nandini Breggin <bregginnr@s.dcsdk12.org>
2020-09-13 14:57:20 -0600

    Exercise 1 Chapter 11: Reference Variables

---
+++ 11/RunTime.java
@@ -0,0 +1,19 @@
+public class RunTime {
+    
+
+    public static void main(String[] args) {
+
+        String str ;
+
+        str = new String("Hello, World!");
+
+        System.out.println(str);
+
+        String secondVariable;
+
+        System.out.println(secondVariable);
+
+        // variable secondVariable might not have been initialized
+        // I created the reference variable, but I didn't create secondVariable as a new String
+    }
+}
