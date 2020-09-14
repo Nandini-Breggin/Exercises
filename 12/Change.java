a6cd56c60c9a8052935fb4d5d251c744e9867d7a
Nandini Breggin <bregginnr@s.dcsdk12.org>
2020-09-13 19:41:29 -0600

    Chapter 12 Exercise 3

--- 12/Change.java
+++ 12/Change.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class CorrectChange {
+public class Change {
 
 	public static void main( String [] args ) {
 		
 		Scanner input = new Scanner( System.in );
 
 		System.out.print( "Input the cents: " );
-		int cents = input.nextInt();
+        int cents = input.nextInt();
+        
+        int dollar = cents / 100;
+        int quarters = ( cents % 100 ) / 25;
+        int dimes = ( ( cents % 100 ) % 25) / 10;
+        int nickels = ( ( ( cents % 100 ) % 25) % 10 ) / 5;
+        int pennies = ( ( ( cents % 100 ) % 25) % 10 ) % 5;
 
-		System.out.println( "Your change is: " + cents / 100 + " dollars and " + ( cents % 100 ) / 25 + " quarters and " + ( ( cents % 100 ) % 25) / 10 + " dimes and " + ( ( ( cents % 100 ) % 25) % 10 ) / 5 + " nickels and " + ( ( ( cents % 100 ) % 25) % 10 ) % 5 + " pennies.") ;
+		System.out.println( "Your change is: " + dollar + " dollars and " + quarters + " quarters and " + dimes + " dimes and " + nickels + " nickels and " + pennies + " pennies.") ;
 
 	}
 
