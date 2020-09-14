b7f0973c33c4d93a54177e8acf47554a32cd3a6f
Nandini Breggin <bregginnr@s.dcsdk12.org>
2020-09-13 19:32:01 -0600

    Chapter 12 Exercise 2

---
+++ 12/Dollar.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Dollar {
+    public static void main(String [] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print( "Input the cents: " );
+		int cents = input.nextInt();
+
+        System.out.println( "That is " + cents / 100 + " dollars and " + cents % 100 + " cents." );
+    }
+}
