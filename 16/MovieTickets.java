fdb4d26cafedf646a865a6a225555feea0c77315
Nandini Breggin <bregginnr@s.dcsdk12.org>
2020-09-17 18:19:25 -0600

    Chapter 16 Exercise 8

--- 16/MovieTickets.java
+++ 16/MovieTickets.java
@@ -13,3 +13,39 @@ There are many ways in which the if statements can be nested.
 Sketch out a flowchart as you design your program.
 **/
 
+import java.util.Scanner;
+import java.lang.Math;
+
+public class MovieTickets {
+    public static void main (String [] args) {
+
+        Scanner input = new Scanner( System.in );
+        
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+
+        System.out.print("Please choose a time. (military time: 1200 - noon) ");
+        
+        System.out.print("Enter the time chosen: ");
+        int time = input.nextInt();
+        double price; // Dollars
+        
+        if((age >= 13) && (time < 1700)) { // 13+ yrs adult & before 5pm is matinee
+            price = 5.00;
+        } 
+        else if ((age <=13 ) && (time < 1700)) { // under 13 yrs child & before 5pm is matinee
+            price = 2.00;
+        } 
+        else if ((age <= 13) && (time >= 1700)) { // under 13 yrs child & after 5pm is not matinee
+            price = 4.00;
+        } 
+        else {
+            price = 8.00; // normal adult ticket cost
+        }
+
+        System.out.println("The price of the movie ticket is: $" + price);
+
+    }
+
+
+}
