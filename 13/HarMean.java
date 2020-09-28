26ef8ace009f5f835b70f925b4ef69e01df3cb0c
Nandini Breggin <bregginnr@s.dcsdk12.org>
2020-09-28 10:44:03 -0600

    Add comments when we went over in class, no change to code

--- 13/HarMean.java
+++ 13/HarMean.java
@@ -3,14 +3,26 @@ import java.lang.Math;
 
 public class HarMean {
     public static void main (String [] args){
+
+        // Resistance to Change - robust
+        // if someone else changes the rest of the code,
+        // it should work smoothly... work properly
+
+
         Scanner input = new Scanner(System.in);
         System.out.print("Enter x: ");
-        int x = input.nextInt();
+        int x = input.nextInt(); // should use double
         System.out.print("Enter y: ");
-        int y = input.nextInt();
+        int y = input.nextInt(); // should use double
+        
+        input.close();
 
-        double arithmetic = (x+y)/2;
-        double harmean = 2/((1.0/x)+(1.0/y));
+
+        // 
+
+
+        double arithmetic = (x + y) / 2;
+        double harmean = 2 / ((1.0/ x ) + (1.0/ y ));
 
         System.out.println("Arithmetic mean: " + arithmetic);
         System.out.println("Harmonic mean: " + harmean);
