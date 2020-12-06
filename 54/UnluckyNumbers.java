472bc61d8a92af331f701405fe1c6dac05e67580
Nandini Breggin <bregginnr@s.dcsdk12.org>
2020-12-05 22:03:45 -0700

    Chapter 54 Exercise 2

---
+++ 54/UnluckyNumbers.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class UnluckyNumbers {
+
+	public static void main(String[] args) {
+		
+        Scanner input = new Scanner(System.in);
+        
+		System.out.print("Enter a number: ");
+        long num = input.nextLong();
+
+        if (num < 0) {
+            // ends code if it is a negative number
+            System.exit(1);
+        }
+            
+        while (num >= 0) {
+            if (isUnlucky(num)) {
+                // if it does contain the number 13 then it is unlucky
+                System.out.println(num + " is unlucky");
+
+            } else if (!(isUnlucky(num))) {
+                // else if it does not contain the number 13 then it is safe
+                System.out.println(num + " is safe");
+                System.out.print("Enter a number: ");
+            }
+        }
+        
+        
+	}
+
+	public static boolean isUnlucky(long num) {
+
+        String numFinder = String.valueOf(num);
+        
+		return numFinder.contains("13");
+	}
+
+}
