c85d20b96f89608aa5babf30f28f945f5cc854cf
Nandini Breggin <bregginnr@s.dcsdk12.org>
2020-12-09 10:30:31 -0700

    Add Chapter 72, Exercise 1

---
+++ 72/Sum.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Sum {
+	
+	public static void main(String [] args) {
+		
+		Scanner userInput = new Scanner(System.in);
+		System.out.print("What is the file name? ");
+
+        File file = new File(userInput.nextLine());
+        // uses the file name inputed by the user
+
+		int sum = 0;
+
+        Scanner in;
+        
+
+		try {
+			in = new Scanner( file );
+			while(in.hasNextInt()) {
+				sum += in.nextInt();
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			System.out.println("Sum: " + sum);
+		}
+
+	}
+
+}
