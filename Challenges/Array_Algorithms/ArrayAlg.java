a74b480809cb8f3d1c7bc309b9d80a56fa556c4f
Nandini Breggin <bregginnr@s.dcsdk12.org>
2021-01-09 09:57:33 -0700

    Array ALg Not done

--- Challenges/Array_Algorithms/ArrayAlg.java
+++ Challenges/Array_Algorithms/ArrayAlg.java
@@ -1,40 +1,33 @@
-import java.util.Arrays;
+// import java.util.Arrays;
 // import java.util.*;
 
 public class ArrayAlg {
     public static void main(String[] args) {
 
-        int haystack[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 333};
+        int[] ogData = { 1, 4, 2, 5, 3, 5, 1010, 234, 23445, 32948 };
 
-        
-        search(haystack, 3); // 9
+        System.out.println("\nSearching haystack:");
+        search(ogData, 0); // 9
         
         System.out.println("\n");
     }
 
     // Returns index of needle found in the haystack
     // Constant or Linear?
-    public static int search(int[] haystack, int needle) {
+    public static int search( int[] haystack, int needle ) {
 
-        int num = 0;
+		for (int i = 0; i < haystack.length; i++) {
 
-        for (int i = 0; i < haystack.length; i++) {
-            
-            // System.out.print(i);
-            if (needle == haystack[i]); {
-                System.out.println("\nSearching haystack:");
-                num++;
-                System.out.println("There is " + num + ", " + needle);
-                System.out.println(i);
-                // return i;
-                
-            }
-            
-            return i;
-    
+			if (haystack[i] == needle) {
+                // System.out.println(i);
+                System.out.println("Test");
+                System.out.println(haystack[i]);
+				return i;
+			}
         }
-        return -1;
         
+		return -1;
+    
     }
 
     // public static int sort(int[] data) {
